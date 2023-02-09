ecc612ea988bb5dcf770d97c854c9baa4ac59743
Sam Zanca <sam.zanca@generalassemb.ly>
2023-02-08 19:43:14 -0500

    Update test.js

--- __tests__/test.js
+++ __tests__/test.js
@@ -1,6 +1,27 @@
 let algos = require("../arrayDiffs.js");
 
-test("Return an array with all the unique numbers (the numbers that are not in both arrays).", () => {
+function pretty(arr) {
+  return JSON.stringify(arr.sort())
+}
+
+test("Should filter out non-duplicates", () => {
   const result = algos.arrayDiffs([1, 44, 2, 3, 5], [33, 1, 2, 3, 4, 5])
-  expect(result).toEqual(expect.arrayContaining([44, 33, 4]));
+  expect(pretty(result)).toBe(pretty([44, 33, 4]));
 });
+
+
+test('Should return second since first is empty', () => {
+  const result = algos.arrayDiffs([], [33, 1, 2, 3, 4, 5])
+  expect(pretty(result)).toBe(pretty([33, 1, 2, 3, 4, 5]));
+})
+
+test('Should return second since first is empty 2', () => {
+  const result = algos.arrayDiffs([33, 1, 2, 3, 4, 5], [])
+  expect(pretty(result)).toBe(pretty([33, 1, 2, 3, 4, 5]));
+})
+
+
+test('Should return empty array', () => {
+  const result = algos.arrayDiffs([1,2,3,4], [1,2,2,3,4])
+  expect(pretty(result)).toBe(pretty([]));
+})
